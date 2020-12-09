dc845ab8b7b6f0de180ce8c99091822b02fec889
Yana <shiro.kirio@gmail.com>
2020-12-10 00:03:54 +0200

    Checkbox validation

--- js/form.js
+++ js/form.js
@@ -1,30 +1,18 @@
-// function validateForm() {
-//   var inputs = document.forms["myForms"]["required"].value;
-//   if (inputs == null || inputs == "") {
-//     document.get;
-//   }
-// }
-
 $("document").ready(function () {
   $(".submit").on("click", function () {
     $(".order-form .required").each(function () {
       let target = $(this);
+      let isValid = target.val() != "";
       // console.log("tag name", $(this).tagName());
       if (this.tagName === "DIV") {
-        target = $(
-          document.querySelector(".order-form div.required").parentElement
-            .firstChild
-        );
-        console.log(
-          document.querySelector(".order-form div.required").parentElement
-            .firstChild
-        );
+        target = document
+          .querySelector(".order-form div.required")
+          .parentElement.querySelector("input");
+        isValid = target.checked;
       }
-      if (target.val() != "") {
-        // Если поле не пустое удаляем класс-указание
+      if (isValid) {
         $(this).removeClass("empty_field");
       } else {
-        // Если поле пустое добавляем класс-указание
         $(this).addClass("empty_field");
       }
     });
