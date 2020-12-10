0d7283a1087319491aea617ab34b466c6ce7fc8d
Yana <shiro.kirio@gmail.com>
2020-12-11 00:05:16 +0200

    Mobile adaptation fix, added phone number pattern validation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
+// Disable buttons
+
 const myButtons = document.querySelectorAll(".no-link");
 myButtons.forEach((button) => {
   button.addEventListener("click", (event) => event.preventDefault());
+});
+
+// Phone inputs for numbers only
+
+document.querySelectorAll("#phone, #extra-phone").forEach(function (element) {
+  element.onkeydown = function (e) {
+    return !/^[А-Яа-яA-Za-z &^%$#@!~`()*+-\/?]$/.test(e.key);
+  };
 });
